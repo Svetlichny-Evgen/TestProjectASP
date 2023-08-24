f541c0e3cacd80b1c60cbd88af7a3fb7135cbb7f
Svetlichny-Evgen <svetlichnyi.e@gmail.com>
2023-08-24 14:41:35 +0300

    Changed the controller for the contacts

--- TestProjectASP/Controllers/ContactsController.cs
+++ TestProjectASP/Controllers/ContactsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TestProjectASP.Models;
 
 namespace TestProjectASP.Controllers
 {
@@ -7,6 +8,15 @@ namespace TestProjectASP.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if (ModelState.IsValid) {
+                return Redirect("/");
+            }
+            return View("Index");
         }
     }
 }
