151a0550eba77a69dcec544d789013d79c395805
Svetlichny-Evgen <svetlichnyi.e@gmail.com>
2023-08-24 14:37:00 +0300

    Validated the page

--- TestProjectASP/Models/Contact.cs
+++ TestProjectASP/Models/Contact.cs
@@ -1,11 +1,32 @@
-﻿namespace TestProjectASP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestProjectASP.Models
 {
     public class Contact
     {
+        [Display(Name ="Введите имя")]
+        [Required(ErrorMessage ="Вам нужно ввести имя")]
         public string Name { get; set; }
+
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
         public string Surname { get; set; }
+
+
+        [Display(Name = "Введите ваш возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести ваш возраст")]
         public int Age { get; set; }
+
+
+        [Display(Name = "Введите ваш аддрес эл.почты")]
+        [Required(ErrorMessage = "Вам нужно ввести ваш аддрес эл.почты")]
         public string Email { get; set; }
+
+
+        [Display(Name = "Введите сообщение, которое вы хотите отправить")]
+        [StringLength(30, ErrorMessage ="Текст не менее 30 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение, которое вы хотите отправить")]
         public string Message { get; set; }
     }
 }
